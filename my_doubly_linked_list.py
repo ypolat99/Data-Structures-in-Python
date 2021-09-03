850ad9814c98fdea5561a61430d46cbb50f81c3b
ypolat99 <77054305+ypolat99@users.noreply.github.com>
2021-09-02 17:30:16 -0700

    Create my_doubly_linked_list.py

---
+++ my_doubly_linked_list.py
@@ -0,0 +1,101 @@
+class Node:
+	def __init__(self, value = None):
+		self.val = value
+		self.next = None
+		self.prev = None
+
+class LinkedList:
+	def __init__(self, value):
+		self.head = Node(value)
+		self.tail = self.head
+		self.len=1
+		
+	def insert_start(self, value):
+		new_node = Node(value)
+		new_node.next = self.head
+		self.head.prev = new_node
+		self.head = new_node
+		self.len += 1
+
+	def insert_end(self, value):
+		new_node = Node(value)
+		new_node.prev = self.tail
+		self.tail.next = new_node
+		self.tail = new_node
+		self.len+=1
+
+	def print_list(self):
+		cur = self.head
+		while cur:
+			print(cur.val)
+			cur = cur.next
+
+	def insert(self, index, value):
+		if index == 0:
+			self.insert_start(value)
+			return
+		elif index >= self.len:
+			self.insert_end(value)
+			return
+		cur = self.head
+		count =0
+		while count <index-1:  #insert 2
+			cur = cur.next # cur idx 1
+			count+=1
+
+		new_node = Node(value)
+		new_node.next = cur.next
+		cur.next.prev = new_node
+		new_node.prev = cur
+		cur.next = new_node
+		self.len+=1
+		
+
+	def remove(self, index):
+		count = 0
+		cur = self.head
+		while count < index -1: #remove 2
+			count+=1  # count 1
+			cur = cur.next  # 5 
+		to_delete = cur.next
+		to_delete.next.prev = cur
+		cur.next = to_delete.next
+		to_delete.next = None
+		to_delete.tail = None
+		self.len-=1
+				
+	def get_len(self):
+		return self.len
+		
+		
+		
+
+# Test Cases
+
+l = LinkedList(5)
+print(l.head.val) #prints 5
+print(l.tail.val) #prints 5
+print("----")
+l.insert_start(4) 
+print(l.head.val) #prints 4
+print(l.tail.val) #prints 5
+print("----")
+l.insert_end(6) 
+l.insert_end(7) 
+print(l.tail.val) #prints 7
+print("----")
+
+l.insert(2,5.5)
+l.print_list() #prints 4,5,5.5,6,7
+
+print("----")
+l.remove(2)
+l.print_list() #prints 4,5,6,7
+print("----")
+print(l.tail.val) #prints 7
+print(l.tail.prev.val) #prints 6
+print("----")
+l.print_list() #prints 4,5,6,7
+l.insert(2,5.5) 
+print("----")
+l.print_list() #prints 4,5,5.5, 6,7
